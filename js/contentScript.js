6cd40f91449326520a00f99fad7937bee58033ee
nicolc <nicolas.l.campos1@gmail.com>
2023-06-14 13:42:21 -0300

    ShowcaseV1

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,3 +1,21 @@
+//FBASE
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js'
+import { getFirestore, getDocs, collection } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAElElfAgw8WubSEdK4CRlrV6BSWPzCfzY",
+  authDomain: "tecnica-9-lanus-proyectos.firebaseapp.com",
+  projectId: "tecnica-9-lanus-proyectos",
+  storageBucket: "tecnica-9-lanus-proyectos.appspot.com",
+  messagingSenderId: "209422117292",
+  appId: "1:209422117292:web:a6d5d944163f1824b83209"
+};
+
+
+const app = initializeApp(firebaseConfig);
+let db = getFirestore(app);
+
+
 const contentBox = document.getElementById('content');
 const div = document.createElement('div');
 const infoBox = document.getElementById('description-box');
@@ -20,23 +38,17 @@ const driveBtn = document.getElementById('driveBtn');
 
 //INICIO Y PINTADO DE PROYECTOS 
 
-document.addEventListener('DOMContentLoaded', () => {
-    const data = getData()
-    .then(data => renderContent(data))
+document.addEventListener('DOMContentLoaded', async () => {
+    const docRef = collection(db, 'proyectos');
+    const docs = await getDocs(docRef)
+    .then((products) => {
+      if (products.size === 0) {
+        console.log('No products');
+      }
+      return products.docs.map((doc) => ({id: doc.id, ...doc.data() })) 
+    })
+    .then(docs => renderContent(docs))
 })
-
-
-
-
-
-const getData = async () => {
-    const res = await fetch('../js/stockTest.json')
-    const data = await res.json()
-    return data
-}
-
-
-
 
 //BUTTON EVENTS
 
--- js/script.js
+++ js/script.js
@@ -2,4 +2,15 @@ const currentYear = document.getElementById('current-year');
 const date = new Date();
 const thisYear = date.getFullYear();
 
+
+
 currentYear.innerHTML = `${thisYear}`
+
+
+const introText = document.getElementsByClassName('intro-text');
+const testBtn = document.getElementById('test-btn');
+
+testBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+})
